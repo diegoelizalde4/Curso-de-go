7e39112d4457eb12e1020d8c991a6f8da75d1ef5
diegoelizalde4 <diegoelizalde475@gmail.com>
2023-07-26 12:16:57 -0600

    first commit

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var nombre string //"Todas las variables tipo string"
+	var edad int
+
+	nombre = "Diego Alejandro"
+	edad = 26
+
+	fmt.Println(edad)
+	fmt.Println(nombre)
+}
